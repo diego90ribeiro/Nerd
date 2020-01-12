7b9789bcb76583bd479970ae961067c8db74a36b
Diego Ribeiro <ribesites@hotmail.com.br>
2020-01-12 20:21:49 -0300

    Layout Responsivo

---
+++ index.php
@@ -0,0 +1,243 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+ <!-- Estilo CSS -->
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<!--JQUERY-->    
+   	<script src="js/jquery.js" type="text/javascript" ></script>
+
+    
+    
+    
+    <!-- IE8 support for HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+    
+
+<link rel="shortcut icon" href="img/logo.png" />
+<title>Distribuidira Milênio</title>
+</head>
+<body>
+<!--Cabeçalho-->
+<header>
+
+<nav class="navbar container col-10 fixed-top mt-2 navbar-expand-lg navbar-light bg-white">
+  <a class="navbar-brand" href="#"><img src="img/logo.png" width="40" height="40"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse justify-content-end " id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link text-dark font-weight-bold click" href="#">HOME</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark font-weight-bold click" href="#">QUEM SOMOS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark font-weight-bold click" href="#">FORNECEDORES</a>
+      </li>
+      <li class="nav-item"> 
+        <a class="nav-link text-dark font-weight-bold click" href="#">TRABALHE CONOSCO</a>
+      </li>
+      <li class="nav-item"> 
+        <a class="nav-link text-dark font-weight-bold click" href="#">CONTATO</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+  
+  
+  
+      
+  
+
+</header>
+<!--Cabeçalho-->
+
+
+<!--Parte 1-->
+  	<div class="container-fluid fundo">  
+            <div class="row vertical-center-top ">
+                <div class="col-md-7 abaixa-top text-center horizontal-center" >
+                    <h1 class=" text-white text-center text-justify font-weight-bold ">BEM-VINDO AO SITE DA DISTRIBUIDORA MILÊNIO</h1>
+                    <h4 class=" text-white font-weight-bold">O lugar certo para encontrar tudo aquilo que voçe precisa para abastecer seu estabelecimento.</h4>              
+           		</div>
+    		</div>
+        </div>
+    <!--Parte 1-->
+    
+    <!--Parte 2-->
+    <div class="container fundo-2 z-2">
+    	<div class="row justify-content-center">
+       <!--  -->
+        <div class=" col-md-5 ml-4 mb-3">
+          <div class="row p-3 col-12 fundo-icon bg-light border border-secondary ">
+            <div class="text-center col-4"><img src="img/icon-quemsomos1.png"></div>
+            <div class="col-8">
+              <h5 class=" text-dark"><b>Diferencial de vendas</b></h5>
+              <p class="my-1">Trabalhamos com equipes de vendas diferenciadas por potencial de mercado, focando totalmente no pequeno varejo, com coordenadores de vendas especificos para este segmento. Contamos com uma equipe de promotores de vendas, que auxiliam nossos clientes, na reposição e layout dos produtos nas gondulas. </p>
+            </div>
+          </div>
+        </div>
+        
+       <!--  -->
+        <div class="col-md-5 ml-4 mb-3">          
+          <div class="row p-3 col-md-12 fundo-icon bg-light border border-secondary ">
+            <div class="text-center c ol-4"><img src="img/icon-quemsomos2.png"></div>
+            <div class="col-8">
+              <h5 class="mb-1 text-dark"><b>Entrega</b></h5>
+              <p class="my-1">Contamos com uma ampla frota de caminhões, o que nos permite trabalhar baseados na logística ágil e eficiente, atendendo com qualidade  e rapidez as necessidades do mercado.</p>
+            </div>
+          </div>
+        </div>
+        
+    <!--  -->
+    	</div>
+    </div>
+    <!--Parte 2-->
+    
+    
+    <!--Parte 3-->
+    <div class="fundo-3 text-center">
+    <h2>Nossos Fornecedores</h2>
+    <hr class="hr">
+    <?php  include 'banner/index.php' ?>
+    </div>
+    
+    
+    
+    <!--Parte 4-->
+     <div class="fundo-4 text-center text-white ">
+    <h2 class="pt-5">CONTATE NOSSA EQUIPE DE ATENDIMENTO </h2>
+    <hr class="hr">
+    <div class="container">
+  <div class="row">
+    <div class="col-sm pt-2 pb-2">
+      <p class="h4"><img src="img/icon-trabalheconosco1.png"> FAX:(67)3366 0024</p>
+    </div>
+    <div class="col-sm pt-2 pb-2">      
+      <p class="h4"><img src="img/icon-trabalheconosco2.png"> TELEFONE:(67)3366 0024</p>
+    </div>
+    <div class="col-sm pt-2 pb-2">
+       <p class="h4"><img src="img/icon-trabalheconosco3.png"> CELULAR:(67)93366 0024</p>
+    </div>
+  </div>
+</div>
+    
+
+     
+  
+
+
+    </div>
+    <!--Parte 4-->
+    
+    
+    <!--Parte 5-->
+     <div class=" container-fluid">
+     <div class="row">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3738.753745045768!2d-54.632938985077146!3d-20.434212586323422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9486e7c7660006c7%3A0x912ab85d7d617767!2sAv.%20Euler%20de%20Azevedo%2C%201316%20-%20Vila%20Nossa%20Sra.%20das%20Gracas%2C%20Campo%20Grande%20-%20MS!5e0!3m2!1spt-BR!2sbr!4v1569167761908!5m2!1spt-BR!2sbr" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+    </div>
+    </div>
+    <!--Parte 5-->
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<footer class="mg position-relative">
+    <div class="bg-secondary text-white">
+	    <div class="container text-capitalize">
+        
+ 
+                
+              <div class="row">
+
+                <div class="col-md-12 mt-3">
+               <nav>
+               <p class="text-center"><img width="40" height="40"  src="img/logo.png"></p>
+                  <ul class="  nav justify-content-center">
+                  <li class="nav-item ">
+                    <a class="nav-link text-white" href="#">HOME</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-white" href="#">QUEM SOMOS</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-white" href="#">FORNECEDORES</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-white" href="#">TRABALHE CONOSCO</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-white" href="#">CONTATO</a>
+                  </li>
+                </ul>
+                </nav>
+                <p class="text-center text-white">
+                <a href="https://www.facebook.com/" target="_new"><img src="img/face.png"></a> 
+                <a href="mailto:ribesites@hotmail.com.br" target="_new"><img src="img/email.png"></a>
+                </p>
+                <p class="text-center text-white">© Milênio 2019, todos os direitos reservados.  - Desenvolvido por DIEGO RIBEIRO. </p>
+        
+      </div>
+    </div>
+  </div>
+</footer>
+  
+  
+  
+<!-- JavaScript -->
+<script src="js/bootstrap.js" type="text/javascript"></script>
+
+
+
+
+                     
+
+   
+    
+
+
+
+
+
+    
+    
+ 
+  </body>
+</html>
